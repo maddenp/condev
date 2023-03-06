c576e315aeecdb1e15cc47ef317727f34eb252b1
Paul Madden <paul.madden@lasp.colorado.edu>
2023-03-06 18:47:08 +0000

    Work on demo tests

--- demo/src/hello/core.py
+++ demo/src/hello/core.py
@@ -3,6 +3,11 @@ A Python/C hello-world demo
 """
 
 
+def hello() -> str:
+    """The first word in a programming trope"""
+    return "hello"
+
+
 def main() -> None:
     """Main entry point"""
-    print("hello")
+    print(", ".join([hello()]))
