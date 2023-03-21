3c6fc3c51e29fd3d5bddf96c783a1af0d2a87b09
Paul Madden <maddenp@colorado.edu>
2023-03-21 11:42:48 -0600

    unset-var-fix (#3)

--- demo/src/setup.py
+++ demo/src/setup.py
@@ -10,16 +10,17 @@ from setuptools import setup  # type: ignore
 with open(os.path.join(os.environ["RECIPE_DIR"], "meta.json"), "r", encoding="utf-8") as f:
     meta = json.load(f)
 
-pkgname = meta["name"]
+name_conda = meta["name"]
+name_py = name_conda.replace("-", "_")
 
 setup(
     entry_points={
         "console_scripts": [
-            "heythere = %s.core:main" % pkgname,
+            "heythere = %s.core:main" % name_py,
         ]
     },
-    name=pkgname,
-    package_data={pkgname: ["resources/conf.json"]},
-    packages=[pkgname],
+    name=name_conda,
+    package_data={name_py: ["resources/conf.json"]},
+    packages=[name_py],
     version=meta["version"],
 )
--- src/setup.py
+++ src/setup.py
@@ -10,16 +10,17 @@ from setuptools import setup  # type: ignore
 with open(os.path.join(os.environ["RECIPE_DIR"], "meta.json"), "r", encoding="utf-8") as f:
     meta = json.load(f)
 
-pkgname = meta["name"]
+name_conda = meta["name"]
+name_py = name_conda.replace("-", "_")
 
 setup(
     entry_points={
         "console_scripts": [
-            "condev-meta = %s.meta:main" % pkgname,
+            "condev-meta = %s.meta:main" % name_py,
         ]
     },
-    name=pkgname,
-    packages=[pkgname],
+    name=name_conda,
+    packages=[name_py],
     scripts=["bash/condev-shell"],
     version=meta["version"],
 )
