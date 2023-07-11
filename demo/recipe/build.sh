d535a6f1544397a0c8e8c1dedef4ee3b8acc3ad4
Paul Madden <maddenp@colorado.edu>
2023-07-11 15:18:24 +0000

    Clear test_files dir during build [skip ci]

--- demo/recipe/build.sh
+++ demo/recipe/build.sh
@@ -5,6 +5,7 @@ python -m pip install -vv .
 (cd world && make install)
 # Copy files needed during test phase:
 dst=../test_files
+rm -fr $dst
 mkdir -pv $dst
 mv -v $PKG_NAME $dst/
 cp -v $SRC_DIR/pyproject.toml $dst/
